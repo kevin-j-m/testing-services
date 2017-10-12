0805d6f749e1dcc18690caf9c3fa7d7bf6616a7d
Kevin Murphy <murphy.kevin.mail@gmail.com>
2017-10-11 22:20:34 -0400

    Use regular stubbing for location spec
    This removes the use of the fake in favor of directly stubbing the
    Weather module. This removes the need to understand the complexity of
    the fake and the API. Instead, it trades that for having the test know
    what the shape of the data is that it expects to be returned from
    `Sweathr::Weather.current_conditions`.

--- spec/sweathr/location_spec.rb
+++ spec/sweathr/location_spec.rb
@@ -4,10 +4,11 @@ require_relative "../support/fake_weather_client"
 RSpec.describe Sweathr::Location do
   describe "#sweater_weather?" do
     context "the current temp feels like it's between 55-65* F" do
-      let(:fake) { Sweathr::Weather::FakeWeatherClient.new(feels_like: "56") }
-
-      before { Sweathr::Weather.client = fake }
-      after { Sweathr::Weather.client = Sweathr::Weather::Api.new }
+      before do
+        allow(Sweathr::Weather).to receive(:current_conditions)
+          .with(zip_code: "02108")
+          .and_return(double(feels_like_f: 56.0))
+      end
 
       it "says it's time to break out the sweater" do
         location = Sweathr::Location.new(zip_code: "02108")
@@ -16,10 +17,11 @@ RSpec.describe Sweathr::Location do
     end
 
     context "the current temp feels less than 55* F" do
-      let(:fake) { Sweathr::Weather::FakeWeatherClient.new(feels_like: "54") }
-
-      before { Sweathr::Weather.client = fake }
-      after { Sweathr::Weather.client = Sweathr::Weather::Api.new }
+      before do
+        allow(Sweathr::Weather).to receive(:current_conditions)
+          .with(zip_code: "02108")
+          .and_return(double(feels_like_f: 54.9))
+      end
 
       it "advises against the sweater" do
         location = Sweathr::Location.new(zip_code: "02108")
@@ -28,10 +30,11 @@ RSpec.describe Sweathr::Location do
     end
 
     context "the current temp feels greater than 65* F" do
-      let(:fake) { Sweathr::Weather::FakeWeatherClient.new(feels_like: "66") }
-
-      before { Sweathr::Weather.client = fake }
-      after { Sweathr::Weather.client = Sweathr::Weather::Api.new }
+      before do
+        allow(Sweathr::Weather).to receive(:current_conditions)
+          .with(zip_code: "02108")
+          .and_return(double(feels_like_f: 66.0))
+      end
 
       it "advises against the sweater" do
         location = Sweathr::Location.new(zip_code: "02108")
