ef54cc0778a9107868b991c079855fc2a0f08a1a
Kevin Murphy <murphy.kevin.mail@gmail.com>
2017-10-14 15:14:04 -0400

    Testing Mode
    This provides a test mode for the overall Weather module. When enabled,
    it injects a fake client as the client. The fake client has the same API
    surface as the API client, but also responds to a new method which can
    be used to save state for the temperatures. Rather than reaching out to
    the API to retrieve the temperature, the fake client refers to an
    internal hash, keyed on by zip code, to find the temperature. That value
    is then provided as the current condition.

--- lib/sweathr/testing/fake_weather_client.rb
+++ lib/sweathr/testing/fake_weather_client.rb
@@ -1,14 +1,18 @@
 module Sweathr
-  module Weather
+  module Testing
     class FakeWeatherClient
       def initialize(results = {})
         @results = results
       end
 
+      def add_condition(zip_code:, temp_f:)
+        @results[zip_code] = temp_f
+      end
+
       def current_conditions(zip_code:)
         {
           "current_observation" => {
-            "feelslike_f" => @results.fetch(:feels_like, "55.5")
+            "feelslike_f" => @results[zip_code]
           }
         }
       end
--- spec/sweathr/location_testing_mode_spec.rb
+++ spec/sweathr/location_testing_mode_spec.rb
@@ -1,43 +1,41 @@
 require "sweathr/location"
+require "sweathr/testing"
 
 RSpec.describe Sweathr::Location do
   describe "#sweater_weather?" do
     context "the current temp feels like it's between 55-65* F" do
-      before do
-        allow(Sweathr::Weather).to receive(:current_conditions)
-          .with(zip_code: "02108")
-          .and_return(double(feels_like_f: 56.0))
-      end
-
       it "says it's time to break out the sweater" do
+        Sweathr::Testing.enable!
+        Sweathr::Weather.client.add_condition(zip_code: "02108", temp_f: "56.0")
+
         location = Sweathr::Location.new(zip_code: "02108")
         expect(location.sweater_weather?).to be true
+
+        Sweathr::Testing.disable!
       end
     end
 
     context "the current temp feels less than 55* F" do
-      before do
-        allow(Sweathr::Weather).to receive(:current_conditions)
-          .with(zip_code: "02108")
-          .and_return(double(feels_like_f: 54.9))
-      end
-
       it "advises against the sweater" do
+        Sweathr::Testing.enable!
+        Sweathr::Weather.client.add_condition(zip_code: "02108", temp_f: "54.9")
+
         location = Sweathr::Location.new(zip_code: "02108")
         expect(location.sweater_weather?).to be false
+
+        Sweathr::Testing.disable!
       end
     end
 
     context "the current temp feels greater than 65* F" do
-      before do
-        allow(Sweathr::Weather).to receive(:current_conditions)
-          .with(zip_code: "02108")
-          .and_return(double(feels_like_f: 66.0))
-      end
-
       it "advises against the sweater" do
+        Sweathr::Testing.enable!
+        Sweathr::Weather.client.add_condition(zip_code: "02108", temp_f: "66.0")
+
         location = Sweathr::Location.new(zip_code: "02108")
         expect(location.sweater_weather?).to be false
+
+        Sweathr::Testing.disable!
       end
     end
   end
