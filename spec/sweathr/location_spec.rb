cf5b51d4efd5e7c56d5b67bfe146802bc9012667
Kevin Murphy <murphy.kevin.mail@gmail.com>
2017-10-15 21:05:31 -0400

    Change variable name of spec

--- spec/sweathr/location_spec.rb
+++ spec/sweathr/location_spec.rb
@@ -3,9 +3,9 @@ require "sweathr/location"
 RSpec.describe Sweathr::Location do
   describe "#sweater_weather?" do
     it "says it's time to break out the sweater" do
-      weather = Sweathr::Location.new(zip_code: "02108")
+      location = Sweathr::Location.new(zip_code: "02108")
 
-      expect(weather.sweater_weather?).to be true
+      expect(location.sweater_weather?).to be true
     end
   end
 end
